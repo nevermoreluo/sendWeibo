ee777ebc7e6d1367e2d6059c55976ec0914bc917
nevermoreluo <656763371@qq.com>
2016-04-22 11:29:34 +0800

    Update config.py

--- config.py
+++ config.py
@@ -12,6 +12,18 @@ Dirname = 'data'
 
 
 
-
-
-
+#是否检查有最新的网络资源，并推送给邮箱(True/False)
+Send_email = True
+#收件人邮箱地址，群发请用逗号隔开,ex: 'xxxx@qq.com,zzz@126.com' 
+To = ''
+#发件人邮箱地址 
+From=''
+#密码 
+Email_password=''
+#发件人邮箱的SMTP服务器地址 
+#126:'smtp.126.com'\163:'smtp.163.com'\qq:'smtp.qq.com'\sina:'smtp.sina.com'\gmail:'smtp.gmail.com' 
+SMTPServer='smtp.qq.com'
+#端口25,587等等
+SMTPPort = 587
+#邮件标题
+Title = 'Rosebush'
